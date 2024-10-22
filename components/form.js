3f68a6f8fba714c8b8242218dec2b226e2013c2b
Thomas Guillet <thomas@palya.fr>
2024-10-22 11:45:24 +0200

    Ajoute le formulaire de témoignages avec les nouveaux fichiers

---
+++ components/form.js
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+export function AppForm() {
+  const [temoignage, setTemoignage] = useState("");
+  const [lieu, setLieu] = useState("");
+  const [nom, setNom] = useState("");
+  const [email, setEmail] = useState("");
+  const [sending, setSending] = useState(false);
+  const [done, setDone] = useState(true);
+  const [error, setError] = useState(false);
+
+  async function publish() {
+    if (temoignage.length == 0) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setSending(true);
+    const supabaseClient = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    );
+
+    const result = await supabaseClient.from("temoignages").insert({
+      histoire: temoignage,
+      lieu,
+      identite: nom,
+      email,
+    });
+    setTemoignage("");
+    setSending(false);
+    setDone(true);
+  }
+
+  return (
+    <>
+      {!done ? (
+        <>
+          <div class="field">
+            <label class="label">Témoignage</label>
+            <div class="control">
+              <textarea
+                class="textarea"
+                value={temoignage}
+                onChange={(e) => setTemoignage(e.target.value)}
+              ></textarea>
+            </div>
+            {error ? (
+              <p class="help is-danger">
+                Ce champs ne peut pas être laissé vide.
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div class="field">
+            <label class="label">Lieu</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                value={lieu}
+                onChange={(e) => setLieu(e.target.value)}
+              />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Nom</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                value={nom}
+                onChange={(e) => setNom(e.target.value)}
+              />
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Adresse email</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <p class="help">
+              Cette adresse email ne sera jamais publiée. Elle nous permettra de
+              vous contacter si nécessaire.
+            </p>
+          </div>
+
+          <div class="section field is-grouped">
+            <div class="control">
+              <button
+                class="button"
+                disabled={sending}
+                onClick={() => publish()}
+              >
+                Envoyer
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <div class="notification">Nous avons bien reçu votre témoignage.</div>
+          <div class="section field is-grouped">
+            <div class="control">
+              <button class="button" onClick={() => setDone(!done)}>
+                Faire un nouveau témoignage
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
---
+++ components/sibbutton.js
@@ -0,0 +1,14 @@
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+
+export function SIBButton() {
+  return (
+    <a
+      className="button is-large"
+      href="https://b6623b61.sibforms.com/serve/MUIFAAy9vF3oQ3NbuIueAqmkPRwaH85AHSjWW-ENOZZ28Bj8iRkSiWqId2ccj0Ql0PnqJCNKm440JIKqbC3OEm8aIaw2OYCt_NBqOjRGyCvaU1owbSWE0jBIVcp0oPCi1MKWQ6cDv8PLxRV0apWTgTTcvwPvHeymbVWoFDu2PYV216-iLEIcMGQaH4ZG8wkmVBq6_UxM6fWmSHyd"
+      target="_blank"
+    >
+      Je veux signer l'appel
+    </a>
+  );
+}
