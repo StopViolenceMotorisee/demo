609b1e05d96e2b1a961bc5808ad2fa4289d94e01
Thomas Guillet <thomas@palya.fr>
2024-10-22 11:48:16 +0200

    Ajoute la configuration supabase en CD

--- components/sibbutton.js
+++ components/sibbutton.js
@@ -8,7 +8,7 @@ export function SIBButton() {
       href="https://b6623b61.sibforms.com/serve/MUIFAAy9vF3oQ3NbuIueAqmkPRwaH85AHSjWW-ENOZZ28Bj8iRkSiWqId2ccj0Ql0PnqJCNKm440JIKqbC3OEm8aIaw2OYCt_NBqOjRGyCvaU1owbSWE0jBIVcp0oPCi1MKWQ6cDv8PLxRV0apWTgTTcvwPvHeymbVWoFDu2PYV216-iLEIcMGQaH4ZG8wkmVBq6_UxM6fWmSHyd"
       target="_blank"
     >
-      Je veux signer l'appel
+      Je veux signer l’appel
     </a>
   );
 }
