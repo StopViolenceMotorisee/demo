6575dc6e978dc1ebb78303e0f2541a52ce59dc21
Thomas Guillet <thomas@palya.fr>
2024-11-08 17:39:01 +0100

    Ajoute une navigation entre les témoignages

--- components/card.js
+++ components/card.js
@@ -1,6 +1,6 @@
-export function Card({ content, context }) {
+export function Card({ id, content, context }) {
   return (
-    <div className="card">
+    <div id={id} className="card">
       <div className="card-content">
         <p className="title is-5">“ {content} ”</p>
         <p className="subtitle is-5">{context}</p>
--- components/cards.js
+++ components/cards.js
@@ -1,21 +1,49 @@
 "use client";
 
 import { Card } from "./card.js";
-import { useEffect } from "react";
+import { useState } from "react";
 import data from "@/verbatims.json";
 
 export function Cards() {
+  const [index, setIndex] = useState(0);
+
+  function scroll(update) {
+    const nv = update(index);
+    setIndex(nv);
+    const el = document.getElementById(`card_${nv}`);
+    el.scrollIntoView({ block: "nearest", inline: "center" });
+  }
+
   return (
-    <div className="cards">
-      {data.records.map((d) => {
-        return (
-          <Card
-            key={d.id}
-            content={d.fields.Court}
-            context={d.fields.Contexte}
-          />
-        );
-      })}
+    <div>
+      <div className="cards">
+        {data.records.map((d, i) => {
+          return (
+            <Card
+              key={d.id}
+              id={`card_${i}`}
+              content={d.fields.Court}
+              context={d.fields.Contexte}
+            />
+          );
+        })}
+      </div>
+      <div className="card-buttons">
+        <button
+          className="button"
+          onClick={() => scroll((i) => i - 1)}
+          disabled={index == 0}
+        >
+          &lt;&lt;
+        </button>
+        <button
+          className="button"
+          onClick={() => scroll((i) => i + 1)}
+          disabled={index == data.records.length - 1}
+        >
+          &gt;&gt;
+        </button>
+      </div>
     </div>
   );
 }
